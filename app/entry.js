33fd8603ac03349f4bfd53ccb899723be0b26691
nantona <byteword@zc4.so-net.ne.jp>
2018-01-23 12:04:02 +0000

    node.jsのcrypto関数をクライアントサイドで実行するように変更

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,6 @@
 'use strict';
 const dc = require('damage-calc');
 document.writeln(
+  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>' +
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
   dc.effectiveDamage(100, 50, 30) + '</p>');
